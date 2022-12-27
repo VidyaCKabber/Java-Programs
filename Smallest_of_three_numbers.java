50f4ae71d3df7508667fed3cbc02872c586321fd
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2022-12-27 23:37:48 +0530

    Create Smallest_of_three_numbers.java

---
+++ Smallest_of_three_numbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Source {
+
+   public static void main(String[] args) {
+
+       int n1, n2, n3;
+       Scanner input = new Scanner(System.in);
+       n1 = input.nextInt();
+       n2 = input.nextInt();
+       n3 = input.nextInt();
+        
+       if(n1 < n2){
+           if(n1 < n3){
+               System.out.println("The smallest number entered is "+n1);
+           } else if(n2 < n3){
+                
+           } else{
+               System.out.println("The smallest number entered is "+n3);
+           }
+       } else if(n2 < n3){
+            System.out.println("The smallest number entered is "+n2);
+       } else{
+            System.out.println("The smallest number entered is "+n3);
+       }
+    }
+}
