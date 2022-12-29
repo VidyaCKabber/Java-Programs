310bdb5ff513bef040cfd194276c8f0e85b4739e
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2022-12-29 20:14:38 +0530

    Create Alphabets.java

---
+++ Alphabets.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Alphabets {
+
+   public static void main(String[] args) {
+       int num = 97;
+       while(num <= 122){
+        System.out.print((char)num+" ");
+        num = num+1;
+       }
+   }
+}
