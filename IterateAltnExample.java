9b6d29357637fecd6e31251250a56aebb2b2a8c1
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-01-29 21:32:49 +0530

    Create IterateAltnExample.java

---
+++ IterateAltnExample.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+class IterateAltnExample{
+
+   public static void main(String[] args) {
+       List<Float> random = new ArrayList<Float>();
+       random.add(2f);
+       random.add(4f);
+       random.add(5f);
+       random.add(10f);
+       random.add(99.9f);    
+       random.add(101f);
+
+       iterateAltn(random);
+   }
+
+   public static void iterateAltn(List<Float> num) {
+       ListIterator<Float> it = num.listIterator();
+       while(it.hasNext()) {
+           it.next();
+           System.out.println(it.next());
+       }
+   }
+}
