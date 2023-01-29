da2ffed0142d6adb822dd960f975913d0d34ccbb
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-01-29 21:28:05 +0530

    Create ForwardIterationUsingListIterator.java

---
+++ ForwardIterationUsingListIterator.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+class ForwardIterationUsingListIterator {
+
+   public static void main(String[] args) {
+       List<Float> random = new ArrayList<Float>();
+       random.add(2f);
+       random.add(4f);
+       random.add(5f);
+       random.add(10f);
+       random.add(99.9f);
+       random.add(101f);
+
+       printList(random);
+   }
+
+   public static void printList(List<Float> arr) {
+       ListIterator <Float> it = arr.listIterator();
+       while(it.hasNext()) {
+           System.out.println(it.next());
+       }
+   }
+}
