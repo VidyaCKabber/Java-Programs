fd8dd06b190a0124c9fd8c883bef195b3748c281
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-03-14 21:56:49 +0530

    Create InsertionsortImplementation.java

---
+++ SortingAlgorithms/InsertionsortImplementation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ class InsertionsortImplementation {
+
+    public static void main(String[] args){ 
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] array = new int[n];
+        
+        for(int i=0; i<n; i++){
+            array[i] = input.nextInt();
+        }
+        InsertionSort(array, n);
+    }
+
+    public static void InsertionSort(int[] array, int size){
+        for(int i=1; i<size; i++){
+                int v = array[i]; 
+                int j = i; 
+                
+                while(j>=1 && array[j-1]>v){   
+                    array[j] = array[j-1]; 
+                    j--; 
+                }
+                array[j]=v; 
+            }
+            for(int i=0; i<size; i++){
+                System.out.println(array[i]);
+            }
+        }
+    }
