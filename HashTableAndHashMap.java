e83a5481f83008bc269c969be5973d461d88ad4a
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-06-04 10:55:52 +0530

    Update HashTableAndHashMap.java

--- HashTableAndHashMap.java
+++ HashTableAndHashMap.java
@@ -11,9 +11,16 @@ class HashTableAndHashMap {
         ht.put(101, "Vijay");
         ht.put(102, "Adrushta");
         
+       
         for(Map.Entry t: ht.entrySet()){
             System.out.println( t.getKey()+" : "+t.getValue());
         }
+        
+        System.out.println(ht.containsKey(102));
+        String a = ht.get(102);
+        System.out.println(a);
+        System.out.println(ht.remove(102));
+        System.out.println(ht.containsKey(102));
         
         //Hash Map
         System.out.println("");
@@ -22,9 +29,16 @@ class HashTableAndHashMap {
         ht.put(100, "Vidya");
         ht.put(101, "Vijay");
         ht.put(102, "Adrushta");
+
         
         for(Map.Entry m: ht.entrySet()){
             System.out.println( m.getKey()+" : "+m.getValue());
         }
+        
+        System.out.println(hm.containsKey(102));
+        String b = ht.get(102);
+        System.out.println(b);
+        System.out.println(ht.remove(102));
+        System.out.println(hm.containsKey(102));
     }
 }
